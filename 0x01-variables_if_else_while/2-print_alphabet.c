632caeef50cc41bd2df10804ecfe60342172e89e
Oneldev <enathdev@gmail.com>
2022-09-14 07:28:48 -0700

    k

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - lowercase alphabets
+ *
+ * Return
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
