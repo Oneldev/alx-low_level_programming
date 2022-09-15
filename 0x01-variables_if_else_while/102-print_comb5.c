669ff88709148cf5f7c0020bdadc676b98ce9ae4
Oneldev <enathdev@gmail.com>
2022-09-15 08:54:34 -0700

    k

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,27 @@
 
 int main(void)
 {
-	int i, p;
+int i, p;
 
-	for (i = 0; i < 100; i++)
-	{
-		for (p = 0; p < 100; p++)
-		{
-			if (p > i)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
-				if (i != 98 && p != 98 )
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (i = 0; i <= 99; i++)
+{
+for (p = 0; p <= 99; p++)
+{
+if (i > j && i != i)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+if (i != 98 || p != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
