08aa04d93869537eb372279752e56bdbd714161e
Oneldev <enathdev@gmail.com>
2022-09-14 17:08:40 -0700

    o

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints someything
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
