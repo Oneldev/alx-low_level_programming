25b636e4cb3ba2e3975fe705384502f86fed8974
Oneldev <enathdev@gmail.com>
2022-09-14 19:58:07 -0700

    o

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - print
  * Return: 0
  */
+
 int main(void)
 {
 	int i, p;
 
-	for (i = 0; 1 < 100; 1++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (p = 0; p < 100; i++)
+		for (p = i + 1; p <= 99; p++)
 		{
-			if (p > i)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+
+			if (i == 98 && p == 99 )
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				putchar(',');
 				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
-				if (i != 98 )
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
